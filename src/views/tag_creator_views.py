0647300d77b6cea6b2f5e2bc14f5012e7ac0bce7
Jota Castro <jose.ribamar13@gmail.com>
2024-02-07 00:14:35 -0300

    feat: Create Controllers

--- src/views/tag_creator_views.py
+++ src/views/tag_creator_views.py
@@ -1,14 +1,19 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     """
         responsabilidade para intereção com HTTP
     """
     def validate_and_create(self, http_request: HttpRequest) -> HttpRequest:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
 
+        formatted_response = tag_creator_controller.create(product_code)
+      
     
         
-        return HttpResponse(status_code=200, body={"hello": "Word"})
+        return HttpResponse(status_code=200, body=formatted_response)
